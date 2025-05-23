f45a35023d85650acc2f842b9d4bb98e60354f97
Neemias Patrocinio <112136373+NeemiasGPatrocinio@users.noreply.github.com>
2025-05-23 15:01:59 -0300

    SIMPLIFICAÇÃO BASKARA
    Um aplicativo capas de retornar os X da função ao se inserir A, B e C.

---
+++ Baskara.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ney.curso.baskara;
+
+/**
+ *
+ * @author neemi
+ */
+public class Baskara {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
